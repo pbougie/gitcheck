c3eb13e43361d71c5420c907b858dc4c774eaf92
Patrick Bougie <patrick@bougie.ca>
2019-06-07 14:39:35 -0400

    Show status even when no changes; add color

--- gitcheck.sh
+++ gitcheck.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 ## gitcheck
-## 2015-Sep-09: initial script.
-## 2017-Apr-06: add exclusions.
+## 2015-Sep-09: Initial script.
+## 2017-Apr-06: Add folder exclusions.
+## 2019-Jun-07: Show status even when no changes.
 
 # Initialize variables
 let repos=0
@@ -19,6 +20,7 @@ do
 done
 
 # Find repositories and loop through them
+echo
 while read folder
 do
   if [ -n "${EXCLUDE}" ] && [[ $folder == *$EXCLUDE* ]]; then
@@ -35,19 +37,22 @@ do
   status=`git status --porcelain`
   if [ -n "${status}" ]
   then # Repository has changes
-    printf "\n===== ${repo}\n\n"
+    printf "\033[1;35m=== ${repo}\033[0m\n"
     git branch -vv && echo
     git status -s && echo
     let repos_changed+=1
   else # Repository has no changes
+    printf "\033[1;36m=== ${repo}\033[0m\n"
+    git branch -vv && echo
     let repos_unchanged+=1
   fi
 
   # Back to original folder
   cd - &> /dev/null
-done < <(find . -type d -name .git)
+done < <(find . -type d -name .git | sort)
 
 # Display tally
-printf "\n${repos} git repositories: "
+printf "${repos} git repositories: "
 printf "${repos_unchanged} are unchanged, "
-printf "${repos_changed} have changes.\n\n"
+printf "${repos_changed} have changes."
+echo && echo
